026e16b4dc6d3c8a36fec196b8673e3b6645b5f9
Rogers Guedes <rogerguedes.ft@gmail.com>
2015-07-20 10:55:12 -0300

    updating my 'bashrc' and 'montar' infos;

--- montarHD.sh
+++ montarHD.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#run 'sudo blkid -s UUID' to get the list of drivers and partitions with teir UUID
 mount UUID=00C314D32E38DB25 /mnt/HD160 -t ntfs -o rw,auto,uid=65534,gid=1001,dmode=775,fmode=775,fmask=0002,dmask=0002
